514ef835514e664f591ac8e7593730f6593e0451
Scott Frew <scott@scotts-mbp.home>
2021-01-06 21:42:46 +0000

    preload game assets into gameScene.js

--- src/gameScene.js
+++ src/gameScene.js
@@ -1,5 +1,10 @@
 import { Scene } from "phaser";
 import logoImg from "./assets/images/logo.png";
+import brick1 from "./assets/images/blue-brick.png";
+import brick2 from "./assets/images/green-brick.png";
+import brick3 from "./assets/images/purple-brick.png";
+import brick4 from "./assets/images/red-brick.png";
+import brick5 from "./assets/images/yellow-brick.png";
 
 class GameScene extends Scene {
   constructor() {
@@ -8,10 +13,17 @@ class GameScene extends Scene {
 
   preload() {
     this.load.image("logo", logoImg);
+    this.load.image("brick1", brick1);
+    this.load.image("brick2", brick2);
+    this.load.image("brick3", brick3);
+    this.load.image("brick4", brick4);
+    this.load.image("brick5", brick5);
   }
 
   create() {
     const logo = this.add.image(400, 300, "logo");
+    this.add.image(200, 200, "brick1");
+    this.add.image(250, 450, "brick2");
   }
 
   update() {}
