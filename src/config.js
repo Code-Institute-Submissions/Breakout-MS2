1f74a29d7d0414fd24e160f2e5fcc8cf1e934c39
Scott Frew <scott@Scotts-MacBook-Pro.local>
2021-01-08 20:12:54 +0000

    update config.js to scale game to center

--- src/config.js
+++ src/config.js
@@ -7,6 +7,10 @@ const config = {
   width: 800,
   height: 600,
   scene: GameScene,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   physics: {
     default: "arcade",
     arcade: {
